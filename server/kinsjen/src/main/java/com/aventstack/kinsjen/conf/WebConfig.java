74a538092cf57f848d32d2fcb3db07dc3f56c53b
Anshoo Arora <anshooarora@gmail.com>
2024-04-09 10:16:25 -0400

    RestController context-path (api)

--- server/kinsjen/src/main/java/com/aventstack/kinsjen/conf/WebConfig.java
+++ server/kinsjen/src/main/java/com/aventstack/kinsjen/conf/WebConfig.java
@@ -1,17 +1,25 @@
 package com.aventstack.kinsjen.conf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("*");
+    }
+
+    @Override
+    public void configurePathMatch(final PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api",
+                HandlerTypePredicate.forAnnotation(RestController.class));
     }
 
 }
