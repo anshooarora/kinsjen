39fbd0ce4ee5280de4c856820afcd307573c7e25
Anshoo Arora <anshooarora@gmail.com>
2024-04-09 10:23:44 -0400

    build.sh (initial)

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+echo "Starting kinsjen build script build.sh"
+
+echo "Checking for node.js installation"
+if which node > /dev/null
+then
+  echo "node.js installed, found version: $(node -v)"
+else
+  echo "node.js installation was not found"
+  echo "Install node.js before running this script"
+  exit 0
+fi
+
+if type -p java; then
+  echo "found java executable in PATH"
+  _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+  echo "found java executable in JAVA_HOME"
+  _java="$JAVA_HOME/bin/java"
+else
+  echo "java installation was not found"
+  echo "Install java before running this script"
+  exit 0
+fi
+
+if [[ "$_java" ]]; then
+  version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+  echo "java version = $version"
+  if [[ ! "$version" > "10" ]]; then
+    echo "java version must be 11 or higher"
+    exit 0
+  fi
+fi
+
+cd client/kinsjen
+echo "Starting npm run build"
+npm run build
+
+echo "Removing src/main/resources/templates/"
+rm -r ../../server/kinsjen/src/main/resources/templates/
+mkdir ../../server/kinsjen/src/main/resources/templates/
+echo "Removing src/main/resources/static/"
+rm -r ../../server/kinsjen/src/main/resources/static/
+
+echo "Copying all artifacts to src/main/resources/static/"
+cp -r dist/kinsjen/browser/ ../../server/kinsjen/src/main/resources/static/
+echo "Moving index.html from /static to /templates"
+mv ../../server/kinsjen/src/main/resources/static/index.html ../../server/kinsjen/src/main/resources/templates/index.html
+
+echo "Entering kinsjen server"
+cd ../../server/kinsjen
+
+echo "Running mvn clean install"
+mvn clean install
+
+echo "Launching kinsjen jar"
+cd target
+java -jar kinsjen-1.0-SNAPSHOT.jar
--- server/kinsjen/src/main/java/com/aventstack/kinsjen/conf/WebConfig.java
+++ server/kinsjen/src/main/java/com/aventstack/kinsjen/conf/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
